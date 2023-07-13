3e1cdc4e6ade14d261abf121a199328022a298b2
ebonatto <ebonatto.dev@gmail.com>
2023-07-12 21:31:54 -0300

    botão tema

--- js/nav.js
+++ js/nav.js
@@ -1,8 +1,9 @@
-const timerButton = document.getElementById("timer-button");
+const themeButton = document.getElementById("theme-button");
 const configButton = document.getElementById("config-button");
 
-const configDiv = document.querySelector(".config");
+const themeDiv = document.querySelector(".theme");
 const timerDiv = document.querySelector(".timer");
+const configDiv = document.querySelector(".config");
 
 // timerButton.addEventListener("click", () => {
 //   document.querySelector(".config").classList.add("hidden");
@@ -10,9 +11,26 @@ const timerDiv = document.querySelector(".timer");
 // });
 
 configButton.addEventListener("click", () => switchTimerConfig());
+themeButton.addEventListener("click", () => switchTimerTheme());
 
 function switchTimerConfig() {
-  document.querySelector(".timer").classList.toggle("hidden");
-  document.querySelector(".config").classList.toggle("hidden");
-  console.log("switch");
+  if (configDiv.classList.contains("hidden")) {
+    configDiv.classList.remove("hidden");
+    timerDiv.classList.add("hidden");
+    themeDiv.classList.add("hidden");
+  } else {
+    configDiv.classList.add("hidden");
+    timerDiv.classList.remove("hidden");
+  }
+}
+
+function switchTimerTheme() {
+  if (themeDiv.classList.contains("hidden")) {
+    themeDiv.classList.remove("hidden");
+    timerDiv.classList.add("hidden");
+    configDiv.classList.add("hidden");
+  } else {
+    themeDiv.classList.add("hidden");
+    timerDiv.classList.remove("hidden");
+  }
 }
